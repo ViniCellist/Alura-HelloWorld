16414b4d4c4ca1de4261b2e80ac7af44584862b2
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-04-30 14:41:43 -0300

    feat menu component

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Inicio from './Paginas/Inicio';
 import SobreMim from './Paginas/SobreMim';
+import Menu from './Components/Menu/index'
 
 function App() {
   return (
     <BrowserRouter>
+      <Menu />
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/sobremim" element={<SobreMim />} />
